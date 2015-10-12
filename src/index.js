a5e607f51b4d14dd97b5bb16fee411f462eb40de
accraze <accraze@gmail.com>
2015-10-11 22:52:39 -0700

    style(all): use javascript standard style
    provide consistent style for project
    
    resolves #4

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
-var reader = require('text2token');
+var reader = require('text2token')
 
-Array.prototype.contains = function(v) {
-    for(var i = 0; i < this.length; i++) {
-        if(this[i] === v) return true;
-    }
-    return false;
-};
+module.exports = Translator
 
-Array.prototype.unique = function() {
-    var arr = [];
-    for(var i = 0; i < this.length; i++) {
-        if(!arr.contains(this[i])) {
-            arr.push(this[i]);
-        }
-    }
-    return arr; 
-}
-
-function Translator(nativeText, foreignText) {
-  if ( nativeText == undefined || foreignText == undefined ) {
-      throw new Error('Native and Foreign Texts are both required!');
+function Translator (nativeText, foreignText) {
+  if (nativeText == undefined || foreignText == undefined) {
+    throw new Error('Native and Foreign Texts are both required!')
   }
 
   this.foreignWords = []
@@ -30,151 +15,163 @@ function Translator(nativeText, foreignText) {
   this.sentencePairs = []
 
   this.probs = {}
-  this.transmissions = {}  // this is t(e|f)
-  this.countef = {}  // this countef
+  this.transmissions = {} // this is t(e|f)
+  this.countef = {} // this countef
   this.totalf = {}
   this.totals = {}
 
-  var convertedData = reader.text2token(nativeText);
-  this.nativeLines = convertedData.lines;
-  this.nativeWords = convertedData.tokens;
+  var convertedData = reader.text2token(nativeText)
+  this.nativeLines = convertedData.lines
+  this.nativeWords = convertedData.tokens
 
-  var convertedForeignData = reader.text2token(foreignText);
-  this.foreignLines = convertedForeignData.lines;
-  this.foreignWords = convertedForeignData.tokens;
-  
-  for(var i = 0; i < this.nativeLines.length; i++) {
-    var pair = [ this.nativeLines[i], this.foreignLines[i] ];
+  var convertedForeignData = reader.text2token(foreignText)
+  this.foreignLines = convertedForeignData.lines
+  this.foreignWords = convertedForeignData.tokens
+
+  for (var i = 0; i < this.nativeLines.length; i++) {
+    var pair = [ this.nativeLines[i], this.foreignLines[i]]
     this.sentencePairs.push(pair)
   }
 }
 
-
-Translator.prototype.train = function() {
-  this._initTransmissions();
-  this._iterateEM(10);
+Translator.prototype.train = function () {
+  this._initTransmissions()
+  this._iterateEM(10)
 }
 
-Translator.prototype._initTransmissions = function() {
+Translator.prototype._initTransmissions = function () {
   var probs = {},
-      transmissions = {};
+    transmissions = {}
 
   for (var i = 0; i < this.nativeWords.length; i++) {
-    var word = this.nativeWords[i];
-    var word_poss = [];
+    var word = this.nativeWords[i]
+    var word_poss = []
 
     // if word is in sentence then...
     for (var j = 0; j < this.nativeLines.length; j++) {
-      var sentence = this.nativeLines[j];
+      var sentence = this.nativeLines[j]
 
       if (sentence.indexOf(word) > -1) {
-        var matching = this.foreignLines[this.nativeLines.indexOf(sentence)];
-        var matches = matching.split(' ');
-        for(var index = 0; index < matches.length; index++) {
+        var matching = this.foreignLines[this.nativeLines.indexOf(sentence)]
+        var matches = matching.split(' ')
+        for (var index = 0; index < matches.length; index++) {
           word_poss.push(matches[index])
         }
       }
     }
 
     // remove duplicates
-    word_poss = word_poss.unique();
+    word_poss = word_poss.unique()
 
     // add probable matches
-    probs[word] = word_poss;
+    probs[word] = word_poss
   }
 
-  this.probs = probs;
+  this.probs = probs
 
   for (var i = 0; i < this.nativeWords.length; i++) {
-    var word = this.nativeWords[i];
-    var word_probs = this.probs[word];
-    
+    var word = this.nativeWords[i]
+    var word_probs = this.probs[word]
 
-    var uniform_prob = 1.0 / word_probs.length;
 
-    var prob_set = {};
+    var uniform_prob = 1.0 / word_probs.length
+
+    var prob_set = {}
 
     for (var k = 0; k < word_probs.length; k++) {
-      var w = word_probs[k];
-      prob_set[w] = uniform_prob;
+      var w = word_probs[k]
+      prob_set[w] = uniform_prob
     }
 
     transmissions[word] = prob_set
   }
-  this.transmissions = transmissions;
+  this.transmissions = transmissions
 }
 
-Translator.prototype._iterateEM = function(count) {
+Translator.prototype._iterateEM = function (count) {
   for (var i = 0; i < count; i++) {
     var totalf = {},
-        countef = {};
+      countef = {}
 
-        for(var j=0; j < this.nativeWords.length; j++) {
-          var word = this.nativeWords[j];
-          
-          if(!this.probs.word)
-            continue;
+    for (var j = 0; j < this.nativeWords.length; j++) {
+      var word = this.nativeWords[j]
+
+      if (!this.probs.word)
+        continue
+      /* istanbul ignore next */
+      var word_probs = this.probs[word]
+      /* istanbul ignore next */
+      var prob_set = {}
+      /* istanbul ignore next */
+      for (var k = 0; k < word_probs.length; k++) {
+        var w = word_probs[k]
+        prob_set[w] = 0
+      }
+      /* istanbul ignore next */
+      countef[word] = count
+      /* istanbul ignore next */
+      totalf[word] = 0
+    }
+
+    this.countef = countef
+    this.totalf = totalf
+
+    // iterate over each sentence pair
+    for (var k = 0; k < this.sentencePairs.length; k++) {
+      var sentence = this.sentencePairs[k]
+      var nativeTokens = sentence[0].split('')
+      var foreignTokens = sentence[1].split('')
+
+      for (var f = 0; f < foreignTokens.length; f++) {
+        this.totals[f] = 0
+        for (var n = 0; n < nativeTokens.length; n++) {
+          if (!this.transmissions.n)
+            continue
           /* istanbul ignore next */
-          var word_probs = this.probs[word]
+          if (!this.transmissions.n.f)
+            continue
           /* istanbul ignore next */
-          var prob_set = {};
+          this.countef[n][f] += this.transmissions[n][f] / this.totals[f]
           /* istanbul ignore next */
-          for (var k = 0; k < word_probs.length; k++) {
-            var w = word_probs[k];
-            prob_set[w] = 0;
-          }
-          /* istanbul ignore next */
-          countef[word] = count;
-          /* istanbul ignore next */
-          totalf[word] = 0;
+          this.totalf[n] += this.transmissions[n][f] / this.totals[f]
         }
 
-        this.countef = countef;
-        this.totalf = totalf;
+      }
 
-        // iterate over each sentence pair
-        for(var k = 0; k < this.sentencePairs.length; k++) {
-          var sentence = this.sentencePairs[k];
-          var nativeTokens = sentence[0].split('');
-          var foreignTokens = sentence[1].split('');
-
-          for (var f = 0; f < foreignTokens.length; f++) {
-            this.totals[f] = 0;
-            for (var n = 0; n < nativeTokens.length; n++) {
-              if(!this.transmissions.n)
-                continue;
-              /* istanbul ignore next */
-              if (!this.transmissions.n.f)
-                continue;
-              /* istanbul ignore next */
-              this.countef[n][f] += this.transmissions[n][f]/this.totals[f];
-              /* istanbul ignore next */
-              this.totalf[n] += this.transmissions[n][f]/this.totals[f];
-            }
-
-          }
-
-          for (var n = 0; n < nativeTokens.length; n++) {
-            if (!this.probs.n)
-              continue;
-            /* istanbul ignore next */
-            var n_prob = self.probs[n];
-            /* istanbul ignore next */
-            for (var f = 0; f < n_prob.length; n++) {
-              this.transmissions[n][f] = this.countef[n][f] / this.totalf[n]
-            }
-          }
+      for (var n = 0; n < nativeTokens.length; n++) {
+        if (!this.probs.n)
+          continue
+        /* istanbul ignore next */
+        var n_prob = self.probs[n]
+        /* istanbul ignore next */
+        for (var f = 0; f < n_prob.length; n++) {
+          this.transmissions[n][f] = this.countef[n][f] / this.totalf[n]
         }
+      }
+    }
   }
 }
 
-Translator.prototype.translate = function(nativeWord) {
+Translator.prototype.translate = function (nativeWord) {
   if (!this.transmissions[nativeWord] || nativeWord === undefined) {
-    throw new Error('No match found!');
+    throw new Error('No match found!')
   }
-
-  return this.transmissions[nativeWord];
+  return this.transmissions[nativeWord]
 }
 
+Array.prototype.contains = function (v) {
+  for (var i = 0; i < this.length; i++) {
+    if (this[i] === v) return true
+  }
+  return false
+}
 
-module.exports = Translator;
+Array.prototype.unique = function () {
+  var arr = []
+  for (var i = 0; i < this.length; i++) {
+    if (!arr.contains(this[i])) {
+      arr.push(this[i])
+    }
+  }
+  return arr
+}
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,58 +1,57 @@
-var expect = require('chai').expect;
-var Translator = require('../src/index.js');
+var expect = require('chai').expect
+var Translator = require('../src/index.js')
 
-describe('machine-translator', function() {
+describe('machine-translator', function () {
   it('should throw an error if not given a native and foreign text', function () {
-    expect(Translator).to.throw('Native and Foreign Texts are both required!');
-  });
+    expect(Translator).to.throw('Native and Foreign Texts are both required!')
+  })
 
   it('should initialize model correctly', function () {
-    var t = new Translator('./tests/data/shortEN.txt', './tests/data/shortDE.txt');
+    var t = new Translator('./tests/data/shortEN.txt', './tests/data/shortDE.txt')
 
-    expect(t.nativeLines).to.include('the dog');
-    expect(t.nativeWords).not.to.have.length(0);
+    expect(t.nativeLines).to.include('the dog')
+    expect(t.nativeWords).not.to.have.length(0)
 
-    expect(t.foreignLines).to.include('der Hund');
-    expect(t.foreignWords).not.to.have.length(0);
+    expect(t.foreignLines).to.include('der Hund')
+    expect(t.foreignWords).not.to.have.length(0)
 
-    expect(t.sentencePairs).not.to.have.length(0);
-  });
+    expect(t.sentencePairs).not.to.have.length(0)
+  })
 
   it('should calculate transmissions', function () {
     var t = new Translator('./tests/data/shortEN.txt', './tests/data/shortDE.txt')
-    expect(t.transmissions).to.be.empty;
+    expect(t.transmissions).to.be.empty
     t.train()
 
-    expect(t.transmissions.the).to.contain({ der: 0.2, Hund: 0.2, die: 0.2, Katze: 0.2, Bus: 0.2 });
-    expect(t.transmissions.cat).to.contain({ die: 0.5, Katze: 0.5 });
-    expect(t.transmissions.dog).to.contain({ der: 0.5, Hund: 0.5 });
-    expect(t.transmissions.bus).to.contain({ der: 0.5, Bus: 0.5 });
-
-  });
+    expect(t.transmissions.the).to.contain({ der: 0.2, Hund: 0.2, die: 0.2, Katze: 0.2, Bus: 0.2 })
+    expect(t.transmissions.cat).to.contain({ die: 0.5, Katze: 0.5 })
+    expect(t.transmissions.dog).to.contain({ der: 0.5, Hund: 0.5 })
+    expect(t.transmissions.bus).to.contain({ der: 0.5, Bus: 0.5 })
+  })
 
   it('should iterate the max likelihood', function () {
-    var t = new Translator('./tests/data/shortEN.txt', './tests/data/shortDE.txt');
-    expect(t.countef).to.be.empty;
-    expect(t.totalf).to.be.empty;
+    var t = new Translator('./tests/data/shortEN.txt', './tests/data/shortDE.txt')
+    expect(t.countef).to.be.empty
+    expect(t.totalf).to.be.empty
     t.train()
-    expect(t.transmissions).not.to.be.empty;
-  });
+    expect(t.transmissions).not.to.be.empty
+  })
 
   it('should return probable translations', function () {
-    var t = new Translator('./tests/data/shortEN.txt', './tests/data/shortDE.txt');
+    var t = new Translator('./tests/data/shortEN.txt', './tests/data/shortDE.txt')
     t.train()
-    
+
     var probableMatches = t.translate('cat')
     console.log(probableMatches)
     expect(probableMatches['Katze']).to.equal(0.5)
-  });
+  })
 
   it('should throw an error if translating a non-matching word', function () {
-    var t = new Translator('./tests/data/shortEN.txt', './tests/data/shortDE.txt');
+    var t = new Translator('./tests/data/shortEN.txt', './tests/data/shortDE.txt')
     t.train()
-    
-    expect(function(){
-      t.translate('cool');
-    }).to.throw('No match found!');
-  });
+
+    expect(function () {
+      t.translate('cool')
+    }).to.throw('No match found!')
+  })
 })
