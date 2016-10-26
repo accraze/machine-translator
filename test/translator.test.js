ced45501f8d50462fd05e6f014db1aa9aa249da2
Hasnae Rehioui <hrehioui@atlassian.com>
2016-10-26 21:34:07 +1100

    #2 remove previous files, adjust tests and test coverage scripts

--- test/translator.test.js
+++ test/translator.test.js
@@ -14,7 +14,7 @@ describe('machine-translator', () => {
   });
 
   it('should initialize model correctly', function () {
-    t.train('./tests/data/shortEN.txt', './tests/data/shortDE.txt');
+    t.train('./test/data/shortEN.txt', './test/data/shortDE.txt');
 
     expect(t.nativeLines).to.include('the dog');
     expect(t.nativeWords).not.to.have.length(0);
@@ -28,7 +28,7 @@ describe('machine-translator', () => {
   it('should calculate transmissions', function () {
     var t = new Translator();
     expect(t.transmissions).to.be.empty;
-    t.train('./tests/data/shortEN.txt', './tests/data/shortDE.txt');
+    t.train('./test/data/shortEN.txt', './test/data/shortDE.txt');
 
     expect(t.transmissions.the).to.contain({
       der: 0.2, Hund: 0.2, die: 0.2, Katze: 0.2, Bus: 0.2
@@ -42,13 +42,13 @@ describe('machine-translator', () => {
     var t = new Translator();
     expect(t.countef).to.be.empty;
     expect(t.totalf).to.be.empty;
-    t.train('./tests/data/shortEN.txt', './tests/data/shortDE.txt');
+    t.train('./test/data/shortEN.txt', './test/data/shortDE.txt');
     expect(t.transmissions).not.to.be.empty;
   });
 
   it('should return probable translations', function () {
     var t = new Translator();
-    t.train('./tests/data/shortEN.txt', './tests/data/shortDE.txt');
+    t.train('./test/data/shortEN.txt', './test/data/shortDE.txt');
     var probableMatches = t.translate('cat');
     console.log(probableMatches);
     expect(probableMatches['Katze']).to.equal(0.5)
